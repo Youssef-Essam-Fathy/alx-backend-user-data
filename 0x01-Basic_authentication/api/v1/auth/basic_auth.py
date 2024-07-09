25ee40be7206f0a98f0888060304c71a4d4f9b1f
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-10 01:32:30 +0300

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -82,7 +82,7 @@ class BasicAuth(Auth):
             return (None, None)
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
-        
+
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
 
