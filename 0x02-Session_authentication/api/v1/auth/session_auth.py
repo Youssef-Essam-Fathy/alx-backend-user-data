8b8256cfcc6ae0435e7e018be9c537379da8ddf7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-11 10:22:45 +0300

    modified

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module of SessionAuth config
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """_summary_
+
+    Args:
+        Auth (_type_): _description_
+    """
+    pass
