aa6de21318e4ea0769e727174156d6e61c8ecfef
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-11 11:35:34 +0300

    modified

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,8 +62,6 @@ def before_request() -> None:
         ]
     ):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     if auth.authorization_header(request) is None:
