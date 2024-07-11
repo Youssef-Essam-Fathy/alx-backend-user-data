793887516dea863473a3d464423f2036bb5c0c17
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-11 11:01:22 +0300

    modified

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -89,6 +89,6 @@ class Auth():
         if request is None:
             return None
         session_name = getenv('SESSION_NAME')
-        if session_name == None:
+        if session_name is None:
             return None
         return request.cookies.get(session_name)
