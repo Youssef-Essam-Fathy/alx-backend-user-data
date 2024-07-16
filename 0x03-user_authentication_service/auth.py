9f828163285b48e3ac8ca5ba5b3d622d26b24f9c
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-17 00:52:55 +0300

    Auth Module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,15 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Checks if the email and password are valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                   password.encode('utf-8'), user.hashed_password
+                   )
+
+        except NoResultFound:
+            return False
