f8eb3c0dd0e8dd72de12c8d21e915d73415917f2
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-16 16:25:48 +0300

    Auth Module

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """Takes in password string argument
+    Returns bytes (salted_hashed)
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
