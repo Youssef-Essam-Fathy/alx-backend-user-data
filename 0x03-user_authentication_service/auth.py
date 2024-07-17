6a37111e17d1315b803dadf241be6de456abfdac
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-17 23:02:37 +0300

    Auth Module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,9 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy the session
+        """
+        user = self._db.update_user(user_id, session_id=None)
+        return user
